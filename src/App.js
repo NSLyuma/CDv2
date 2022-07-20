27ad92f3a5f1573c6e7927ed8671d3ecf400f360
NSLyuma <iinokayo@mail.ru>
2022-07-20 23:07:02 +0300

    Created app cover

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h3>Добавить новый продукт</h3>
+      <form>
+        <input placeholder="Название продукта" />
+        <input placeholder="Ккал на 100 г" />
+        <button>Добавить</button>
+      </form>
+      <h3>Добавить запись</h3>
+      <form>
+        <input type="date" />
+        <select>
+          <option value="product">Продукт</option>
+        </select>
+        <input placeholder="Вес (г)" />
+        <button>Записать</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
